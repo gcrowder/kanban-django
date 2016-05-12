204fdf612d92b2af869315e3cd1087425368328d
gcrowder <gregory.j.crowder@gmail.com>
2016-05-12 15:24:46 -0400

    Add model Task and serializer TaskSerializer.

--- kanban/api/models.py
+++ kanban/api/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    status_choices = (
+                ('Back Burner', 'Back Burner'),
+                ('On Deck', 'On Deck'),
+                ('In Process', 'In Process'),
+                ('Complete', 'Complete'))
+    priority_choices = (
+                ('Glacial', 'Glacial'),
+                ('Sloth', 'Sloth'),
+                ('Snail', 'Snail'),
+                ('Rabbit', 'Rabbit'),
+                ('The Flash', 'The Flash'))
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    status = models.CharField(max_length=20, choices=status_choices)
+    priority = models.CharField(max_length=15, choices=priority_choices)
