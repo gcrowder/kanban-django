42f718ace55973173752bc28b30a1dca65218295
gcrowder <gregory.j.crowder@gmail.com>
2016-05-12 15:29:50 -0400

    Add serializer view and appropriate URLs.

--- kanban/api/views.py
+++ kanban/api/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Task
+from .serializers import TaskSerializer
 
-# Create your views here.
+
+class TaskViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows abilities to be viewed or edited.
+    """
+    queryset = Task.objects.all().order_by('id')
+    serializer_class = TaskSerializer
